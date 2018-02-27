8fc3fc5f76d2029a253a7ab96bb448404ffff19c
hd0690 <mehta.harsh03@gmail.com>
2018-02-26 22:11:26 -0800

    Deleted Truck Permit Doctype Added Module Def Config for Transport Module

---
+++ transport/config/transport.py
@@ -0,0 +1,134 @@
+from __future__ import unicode_literals
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"label": _("Master"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Truck Master",
+				},
+				{
+					"type": "doctype",
+					"name": "Driver Master",
+				},
+				{
+					"type": "doctype",
+					"name": "Khalasi Master",
+				},
+				{
+					"type": "doctype",
+					"name": "Truck Parts Inventory",
+				},
+				{
+					"type": "doctype",
+					"name": "Place",
+				},
+				{
+					"type": "doctype",
+					"name": "Item",
+				},
+				{
+					"type": "doctype",
+					"name": "Customer",
+				},
+				{
+					"type": "doctype",
+					"name": "Supplier",
+				},
+				{
+					"type": "doctype",
+					"name": "Sales Partner",
+				},
+				{
+					"type": "doctype",
+					"name": "Address",
+				},
+				{
+					"type": "doctype",
+					"name": "Contact",
+				},
+				{
+					"type": "doctype",
+					"name": "Part Creation Tool",
+				},
+				{
+					"type": "doctype",
+					"name": "Truck Model",
+				},
+				{
+					"type": "doctype",
+					"name": "Truck Type",
+				},
+				{
+					"type": "doctype",
+					"name": "Truck Manufacturer",
+				},
+				{
+					"type": "doctype",
+					"name": "Truck Part Company",
+				},
+
+			]
+		},
+		{
+			"label": _("Trips"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Indent"
+				},
+				{
+					"type": "doctype",
+					"name": "Truck Engagement Form"
+				},
+				{
+					"type": "doctype",
+					"name": "Trip"
+				},
+				{
+					"type": "doctype",
+					"name": "Maintenance Log"
+				},
+
+			]
+		},
+		{
+			"label": _("Documents"),
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Insurance Policy"
+				},
+				{
+					"type": "doctype",
+					"name": "Insurance Companies"
+				},
+				{
+					"type": "doctype",
+					"name": "Truck Papers"
+				},
+
+			]
+		},
+		{
+			"label": _("Reports"),
+			"items": [
+				{
+					"type": "report",
+					"name": "Monthly Maintenance",
+					"doctype": "Maintenance Log",
+					"is_query_report": True
+				},
+				{
+					"type": "report",
+					"name": "Truck wise maintenance",
+					"doctype": "Maintenance Log",
+					"is_query_report": True
+				},
+				
+			]
+		}
+	]
